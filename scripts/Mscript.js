e11b7010ede126a289cb6d3863b6906db605255c
CreatorOfBiontium <johnsonpe83@gmail.com>
2023-04-12 18:29:01 +0200

    ALFA

--- scripts/Mscript.js
+++ scripts/Mscript.js
@@ -13,3 +13,15 @@ var currentScrollPos = window.pageYOffset;
 const myImage = document.getElementById('pfp');
 myImage.addEventListener('click', () => {
   window.location.href = 'https://creatorofbiontium.github.io/PotatoeCloud/login';});
+
+
+
+function altclose(){
+  if (confirm("Tento web je ve velmi rané fázi vývoje. Zda-li souhlasíte s ALFA přístupem budete moci se podívat na web podívat (Skoro nic tu není).")) {
+    
+  } else {
+    window.close()
+  }
+
+
+}
