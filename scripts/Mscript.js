9ead53cd883e25a47893467608ad751840c5241d
CreatorOfBiontium <johnsonpe83@gmail.com>
2023-04-13 18:46:57 +0200

    MORE IN ALERT xd

--- scripts/Mscript.js
+++ scripts/Mscript.js
@@ -15,7 +15,7 @@ myImage.addEventListener('click', () => {
   window.location.href = 'https://creatorofbiontium.github.io/PotatoeCloud/login';});
 
 function altclose(){
-  if (confirm("Tento web je ve velmi rané fázi vývoje. Zda-li souhlasíte s PRE-ALFA přístupem budete moci se podívat na web podívat (Skoro nic tu není).")) {
+  if (confirm("Tento web je ve velmi rané fázi vývoje. Zda-li souhlasíte s PRE-ALFA přístupem budete moci se podívat na web podívat (Skoro nic tu není). POZN.: Tento web se momentálně vyvyjí 'ofline' a budoucí změny nebudou publikovány do dokončení ALFY.")) {
     
   } else {
     window.close()
